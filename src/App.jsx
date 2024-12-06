db1521ffd87f63adeea4d8ffd84db7c1a097e265
salcrom <seya.28@gmail.com>
2024-12-06 09:50:22 +0100

    Fin teoria - cambio a rama Ejercicios 2.1 a 2.5

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import { useState } from "react";
+import Note from "./components/Note";
 
-function App() {
+const App = ({ notes }) => {
+    // const { notes } = props;
+
     return (
-        <>
-            <div>Hola Mundo</div>
-        </>
+        <div>
+            <h1>Notes</h1>
+            <ul>
+                {notes.map((note) => (
+                    <Note key={note.id} note={note} />
+                ))}
+            </ul>
+        </div>
     );
-}
+};
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,25 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+const notes = [
+    {
+        id: 1,
+        content: "HTML is easy",
+        important: true,
+    },
+    {
+        id: 2,
+        content: "Browser can execute only JavaScript",
+        important: false,
+    },
+    {
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        important: true,
+    },
+];
+
+// const result = notes.map((note) => note.content);
+// console.log(result);
+
+createRoot(document.getElementById("root")).render(<App notes={notes} />);

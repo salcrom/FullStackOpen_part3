56e720d48110d2d84e5f0b07900f986de39c5cd1
salcrom <seya.28@gmail.com>
2024-12-06 10:01:49 +0100

    Inicio Informacion curso paso 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
-import Note from "./components/Note";
+const App = () => {
+    const course = {
+        id: 1,
+        name: "Half Stack application development",
+        parts: [
+            {
+                name: "Fundamentals of React",
+                exercises: 10,
+                id: 1,
+            },
+            {
+                name: "Using props to pass data",
+                exercises: 7,
+                id: 2,
+            },
+            {
+                name: "State of a component",
+                exercises: 14,
+                id: 3,
+            },
+        ],
+    };
 
-const App = ({ notes }) => {
-    // const { notes } = props;
-
-    return (
-        <div>
-            <h1>Notes</h1>
-            <ul>
-                {notes.map((note) => (
-                    <Note key={note.id} note={note} />
-                ))}
-            </ul>
-        </div>
-    );
+    return <Course course={course} />;
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,4 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
-const notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        important: true,
-    },
-    {
-        id: 2,
-        content: "Browser can execute only JavaScript",
-        important: false,
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        important: true,
-    },
-];
-
-// const result = notes.map((note) => note.content);
-// console.log(result);
-
-createRoot(document.getElementById("root")).render(<App notes={notes} />);
+createRoot(document.getElementById("root")).render(<App />);

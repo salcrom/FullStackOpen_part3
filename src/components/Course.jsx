a9497457636e2939e9d92b0f9f2806938ebd0b2d
salcrom <seya.28@gmail.com>
2024-12-08 07:57:38 +0100

    Ejercicio 2.5 - paso 10 - listo

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -2,7 +2,7 @@ import { Header } from "./course/Header";
 import { Content } from "./course/Content";
 
 const Course = ({ course }) => {
-    // console.log(course);
+    console.log(course);
     const { name, parts } = course;
     // console.log({ name, parts });
 

a76187a58758d089c32540855be7c74d1033abc8
salcrom <seya.28@gmail.com>
2024-12-11 13:44:49 +0100

    main - fin teoria parte 2.d - pase a rama phonebook

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,80 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 import Note from "./components/Note";
+import noteService from "./services/notes";
 
 const App = () => {
     const [notes, setNotes] = useState([]);
     const [newNote, setNewNote] = useState("");
     const [showAll, setShowAll] = useState(true);
 
-    const hook = () => {
-        console.log("effect");
-        axios.get("http://localhost:3001/notes").then((response) => {
-            console.log("promise fulfiled");
-            setNotes(response.data);
+    useEffect(() => {
+        noteService.getAll().then((initialNotes) => {
+            setNotes(initialNotes);
+        });
+    }, []);
+
+    const handleNoteChange = (event) => {
+        setNewNote(event.target.value);
+    };
+
+    const notesToShow = showAll
+        ? notes
+        : notes.filter((note) => note.important);
+
+    // console.log("render", notes.length, "notes");
+    const toggleImportanceOf = (id) => {
+        // console.log(`importance of ${id} needs to be toggled`);
+        const note = notes.find((n) => n.id === id);
+        const changedNote = { ...note, important: !note.important };
+
+        noteService
+        .update(id, changedNote).then((returnedNote) => {
+            setNotes(
+                notes.map((note) => (note.id !== id ? note : returnedNote))
+            );
+        })
+        .catch(error => {
+            alert(`the note '${note.content}' was already deleted from server`)
+            setNotes(notes.filter(n => n.id !== id))
         });
     };
 
-    useEffect(hook, []);
+    const addNote = (event) => {
+        event.preventDefault();
+        const noteObject = {
+            content: newNote,
+            important: Math.random() > 0.5,
+        };
 
-    console.log("render", notes.length, "notes");
+        noteService.create(noteObject).then((returnedNote) => {
+            console.log(response);
+            setNotes(notes.concat(returnedNote.data));
+            setNewNote("");
+        });
+    };
 
     return (
         <div>
             <h1>Notes</h1>
+            <div>
+                <button onClick={() => setShowAll(!showAll)}>
+                    show {showAll ? "important" : "all"}
+                </button>
+            </div>
             <ul>
-                {notes.map((note) => (
-                    <Note key={note.id} note={note} />
+                {notesToShow.map((note, i) => (
+                    <Note
+                        key={i}
+                        note={note}
+                        toggleImportance={() => toggleImportanceOf(note.id)}
+                    />
                 ))}
             </ul>
+            <form onSubmit={addNote}>
+                <input value={newNote} onChange={handleNoteChange} />
+                <button type="submit">save</button>
+            </form>
         </div>
     );
 };
